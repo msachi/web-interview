a4bbd299894c78ee6636dc1ed2842f942600f25c
msachi <marchee88@gmail.com>
2019-06-10 12:09:51 +0100

    UserInfo component

---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './UserInfo.scss'
+
+const UserInfo = props => {
+  const name = `${props.userInfo.firstName} ${props.userInfo.lastName}`
+
+  return (
+    <div className="component-wrapper">
+      <div className="avatar-wrapper">
+        <img className="avatar" src={props.userInfo.avatar} alt="User avatar" />
+        <p className="username">{name}</p>
+      </div>
+      <p className="change-link">Change</p>
+    </div>
+  )
+}
+
+UserInfo.propTypes = {
+  userInfo: PropTypes.Object,
+}
+
+export default UserInfo
