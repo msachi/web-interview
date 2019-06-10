3f090bd2682a330d59706aed6eaa4ac58e1f7c44
msachi <marchee88@gmail.com>
2019-06-10 14:09:00 +0100

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitForElement,
+  wait,
+} from '@testing-library/react'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+afterEach(() => {
+  cleanup()
+  jest.clearAllMocks()
+})
+
+test('Renders correct options and submits correct appointment data', () => {
+  const { getByText, getByPlaceholderText } = render(<App />)
+
+  waitForElement(() => getByText('Specialist')).then(() => {
+    const specialistButton = getByText('Specialist')
+    const timeButton = getByText('1st Dec at 14:16')
+    const audioButton = getByText('Audio')
+    const notesBox = getByPlaceholderText('Describe your symptoms')
+    const submitButton = getByText('Book appointment')
+
+    fireEvent.click(specialistButton)
+    fireEvent.click(timeButton)
+    fireEvent.click(audioButton)
+    fireEvent.change(notesBox, { target: { value: 'I have a terrible fever' } })
+    fireEvent.click(submitButton)
+  })
+})
