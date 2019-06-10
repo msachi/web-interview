e9ba70765b2a54b228f521788c165492f9fddfaa
msachi <marchee88@gmail.com>
2019-06-10 16:10:45 +0100

    App top header & styling fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import moment from 'moment'
 
-import logo from './logo.png'
+import burger from './images/burger.png'
+import logo from './images/logo.png'
+
 import { API_ENDPOINT } from './config'
 
 import UserInfo from './components/UserInfo/UserInfo'
@@ -70,6 +72,11 @@ class App extends Component {
       .catch(() => this.setState({ error: true, loadingUser: false }))
   }
 
+  getUserInitials() {
+    const { firstName, lastName } = this.state.userInfo
+    return firstName.charAt(0).toUpperCase() + lastName.charAt(0).toUpperCase()
+  }
+
   formatTitle(title) {
     if (title === 'gp') return 'GP'
     return title.charAt(0).toUpperCase() + title.slice(1)
@@ -121,9 +128,13 @@ class App extends Component {
     return (
       <div className="app">
         <div className="app-header">
+          <img src={burger} className="burger-menu" alt="Menu" />
           <img src={logo} className="app-logo" alt="Babylon Health" />
+          {!this.state.error && (
+            <div className="user-initials">{this.getUserInitials()}</div>
+          )}
         </div>
-        <div style={{ maxWidth: 600, margin: '24px auto' }}>
+        <div className="appointment-section">
           <h2 className="h6">New appointment</h2>
           {this.state.error ? (
             <p>
@@ -167,6 +178,7 @@ class App extends Component {
                   text={this.state.noteText}
                   onTextChange={noteText => this.setState({ noteText })}
                 />
+                <div className="section-border" />
                 <SubmitButton title="Book appointment" />
               </form>
             </Fragment>
